61d442f1ab36dbd3bde1a8cda3cb0f0745812901
bxtkezhan <bxtkezhan@qq.com>
2017-10-20 14:07:39 +0800

    规定点击范围

--- src/sceneBase/sceneBox.gd
+++ src/sceneBase/sceneBox.gd
@@ -2,13 +2,26 @@ extends Spatial
 
 export(NodePath) var targetPath
 var target = null
+var active = false
 
 func _ready():
 	if targetPath != null:
 		target = get_node(targetPath)
 
-func _on_stickersBody_input_event( camera, event, click_pos, click_normal, shape_idx ):
+func _on_Area_body_enter( body ):
 	if target != null:
+		if body.get_name() == target.get_name():
+			active = true
+
+
+func _on_Area_body_exit( body ):
+	if target != null:
+		if body.get_name() == target.get_name():
+			active = false
+
+
+func _on_stickersBody_input_event( camera, event, click_pos, click_normal, shape_idx ):
+	if active:
 		if event.type == InputEvent.MOUSE_BUTTON:
 			if event.button_index == BUTTON_RIGHT and event.is_pressed():
 				target.destination = click_pos
